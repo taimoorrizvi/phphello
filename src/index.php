83febc38b03635b8bdddaf1f940db92cd378e021
Taimoor Rizvi <taimoor.rizvi@bankalfalah.com>
2021-12-28 13:43:02 +0500

    Changes made

--- src/index.php
+++ src/index.php
@@ -1,7 +1,23 @@
 <?php
 
-echo "Hello, World from Docker! <br>";
-echo "Hello ECS! <br>";
-echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
+echo "Hello, World from Docker - PHP! <br>";
+
+echo "Now connection have to be made with mysql on remote location <br><br><br>";
+
+echo "******************************************************************";
+
+/* Attempt MySQL server connection. Assuming you are running MySQL
+
+server with default setting (user 'root' with no password) */
+$link = mysqli_connect("localhost", "root", "");
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+ 
+// Print host information
+echo "Connect Successfully. Host info: " . mysqli_get_host_info($link);
+
 
 ?>
